1c2db4a08b3595ec8eaa9f61d8845564de00e935
Yuri Faria <yuri.yurifaria@gmail.com>
2024-02-27 22:45:51 -0300

    is redis on verify

--- src/jobs/index.ts
+++ src/jobs/index.ts
@@ -3,7 +3,7 @@ import syncDefaultSubjectsJupiterWorker from '../subject/jobs/syncDefaultSubject
 import syncSubjectsJupiterWorker from '../subject/jobs/syncSubjectsJupiterByInstitute'
 
 const startJobs = () => {
-  if (process.env.IS_REDIS_ON) {
+  if (!!process.env.IS_REDIS_ON) {
     syncSubjectsJupiterWorker()
     syncDefaultSubjectsJupiterWorker()
     syncClassesJupiterBySubjectWorker()
