242ca828555a02d0a5cf74b427db11786477b6ca
Yuri Faria <yuri.yurifaria@gmail.com>
2024-03-04 20:04:51 -0300

    feat: filter search subjects by subjectclass length

--- src/subject/services/searchSubjects.ts
+++ src/subject/services/searchSubjects.ts
@@ -1,25 +1,27 @@
 import prisma from '../../db'
 
 const searchSubjects = async (search: string) => {
-  const subjects = await prisma.subject.findMany({
-    where: {
-      OR: [
-        {
-          name: {
-            contains: String(search),
-            mode: 'insensitive',
+  const subjects = (
+    await prisma.subject.findMany({
+      where: {
+        OR: [
+          {
+            name: {
+              contains: String(search),
+              mode: 'insensitive',
+            },
           },
-        },
-        {
-          code: {
-            contains: String(search),
-            mode: 'insensitive',
+          {
+            code: {
+              contains: String(search),
+              mode: 'insensitive',
+            },
           },
-        },
-      ],
-    },
-    include: { subjectClass: true, institute: true },
-  })
+        ],
+      },
+      include: { subjectClass: true, institute: true },
+    })
+  ).filter((subject) => subject.subjectClass.length > 0)
 
   return subjects
 }
