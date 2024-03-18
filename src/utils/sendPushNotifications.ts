a7a5d7833bdf3f59f9ab46f236d2d84933dad3cf
Yuri Faria <yuri.yurifaria@gmail.com>
2024-03-18 16:33:35 -0300

    out

--- src/utils/sendPushNotifications.ts
+++ src/utils/sendPushNotifications.ts
@@ -1,32 +1,3 @@
-import Expo, { ExpoPushMessage } from './ExpoClient'
-
-async function sendPushNotifications(pushTokens: string[], title: string, body: string, data?: any): Promise<void> {
-  let expo = new Expo({ useFCM: true })
-
-  let messages: ExpoPushMessage[] = []
-  for (let pushToken of pushTokens) {
-    if (!Expo.isExpoPushToken(pushToken)) continue
-
-    messages.push({
-      to: pushToken,
-      sound: 'default',
-      title,
-      body,
-      data,
-    })
-  }
-
-  let chunks = expo.chunkPushNotifications(messages)
-  let tickets = []
-
-  for (let chunk of chunks) {
-    try {
-      let ticketChunk = await expo.sendPushNotificationsAsync(chunk)
-      tickets.push(...ticketChunk)
-    } catch (error: any) {
-      console.error(`[ERROR] [Send Push Notifications] Unexpected Send Push Notifications: ${error.message}`)
-    }
-  }
-}
+async function sendPushNotifications(pushTokens: string[], title: string, body: string, data?: any): Promise<void> {}
 
 export default sendPushNotifications
