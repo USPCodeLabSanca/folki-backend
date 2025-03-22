e0c4d34cc6579878d670318f893082cd875e5e8c
Yuri Faria <yuri.yurifaria@gmail.com>
2025-03-22 10:16:15 -0300

    Update getMeSubjects.ts

--- src/user/controllers/getMeSubjects.ts
+++ src/user/controllers/getMeSubjects.ts
@@ -10,11 +10,24 @@ const getMeSubjects = async (req: Request, res: Response) => {
   delete body.securePin
 
   try {
+    const latestClass = await prisma.subject_class.findFirst({
+      orderBy: [{ year: 'desc' }, { semester: 'desc' }],
+      select: { year: true, semester: true },
+    });
+    
     const userSubjects = await prisma.user_subject.findMany({
-      where: { userId: user!.id, deletedAt: null },
+      where: {
+        userId: user!.id,
+        deletedAt: null,
+        subjectClass: {
+          year: latestClass.year,
+          semester: latestClass.semester,
+        },
+      },
       include: { subjectClass: { include: { subject: true } } },
       orderBy: { subjectClass: { subject: { name: 'asc' } } },
     })
+    
     res.send({ userSubjects })
   } catch (error: any) {
     console.error(`[ERROR] [User Get Me Subjects] Unexpected User Get: ${error.message}`)
